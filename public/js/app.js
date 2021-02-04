b30957002b53feff4efb682af2bb3eb31a423f03
jquaglia <jqugali@gmail.com>
2021-02-04 10:08:34 -0800

    completed mobile styling

--- public/js/app.js
+++ public/js/app.js
@@ -2,7 +2,7 @@
 
 //this is the keystone of our project
 function addFooter() {
-  $('footer').append('<section>This app uses standard GET and POST methods to retrieve information from API URL\'s it\'s provided. Use API URL\'s that contain API keys at your ownrisk and DO NOT SAVE THEM to the database.</section>');
+  $('footer').append('<section>This app uses standard GET and POST methods to retrieve information from API URL\'s it\'s provided. Use API URL\'s that contain API keys at your own risk and DO NOT SAVE THEM to the database.</section>');
 
 }
 
